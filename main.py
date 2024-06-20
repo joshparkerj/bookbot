6eb62ed9d01bd921488a34029976b55101658488
Josh Parker <joshBBparker@gmail.com>
2024-06-19 21:02:48 -0600

    refactor file read into its own function

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 def main():
  file_name = './books/frankenstein.txt'
+ file_contents = get_file_contents(file_name)
+ line_count = len(file_contents.split('\n')) - 1
+ word_count = len(file_contents.split())
+ char_count = len(file_contents)
+ print(f'\t{line_count}\t{word_count}\t{char_count}\t{file_name}')
+
+def get_file_contents(file_name):
  with open(file_name) as f:
-  file_contents = f.read() 
-  #  print(file_contents)
-  line_count = len(file_contents.split('\n')) - 1
-  word_count = len(file_contents.split())
-  char_count = len(file_contents)
-  print(f'\t{line_count}\t{word_count}\t{char_count}\t{file_name}')
+  return f.read()
 
 main()
